2d9c7bfb9231a362dfd6cc89d073922599b7f8a0
yeongbin1999 <yeongbin1999@gmail.com>
2025-09-17 14:37:39 +0900

    fix

--- backend/src/test/kotlin/com/buddy/BuddyApplicationTests.kt
+++ backend/src/test/kotlin/com/buddy/BuddyApplicationTests.kt
@@ -2,9 +2,7 @@ package com.buddy
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@ActiveProfiles("test")
 @SpringBootTest
 class BuddyApplicationTests {
 
